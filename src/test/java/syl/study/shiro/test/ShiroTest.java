cf1e3f9499ad63e2c49d902f416f6999d7722ebd
史彦磊 <956469025@qq.com>
2016-10-29 00:02:20 +0800

    create project

---
+++ src/test/java/syl/study/shiro/test/ShiroTest.java
@@ -0,0 +1,30 @@
+package syl.study.shiro.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
+/**
+ * Created by Think on 2016/10/28.
+ */
+public class ShiroTest {
+
+
+    @Test
+    public void testShrio(){
+        IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:users.ini");
+        org.apache.shiro.mgt.
+        SecurityManager manager = factory.getInstance();
+        SecurityUtils.setSecurityManager(manager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        subject.login(token);
+
+        System.out.println(subject.isAuthenticated());
+    }
+
+
+}
